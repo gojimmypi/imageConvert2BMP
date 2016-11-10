9ad846bb24ae98897ffd148b37a53d61917b2a4b
jscarletta <jscarletta@HA16081>
2016-11-09 16:13:17 -0800

    Add project files.

---
+++ imageConvert2BMP/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Drawing;
+using System.IO;
+
+namespace imageConvert2BMP
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String appDirectory = HttpContext.Current.Request.PhysicalPath;
+            String targetImageName = HttpContext.Current.Request.QueryString["targetImageName"];
+            if (targetImageName != "")
+            {
+                String imageLocalFullPath = appDirectory + "\\" + targetImageName;
+                if (File.Exists(imageLocalFullPath))
+                {
+                    Image thisImage = Image.FromFile(imageLocalFullPath);
+                    thisImage.Save(HttpContext.Current.Response.OutputStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                }
+                else
+                {
+                    HttpContext.Current.Response.Write("File not found");
+                }
+            }
+            {
+                HttpContext.Current.Response.Write("File not specified");
+            }
+        }
+    }
+}
---
+++ imageConvert2BMP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="imageConvert2BMP.Global" Language="C#" %>
