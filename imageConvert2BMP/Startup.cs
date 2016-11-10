ad0bebc24c860dfadf1257d16578d197d27c67e8
jscarletta <jscarletta@HA16081>
2016-11-09 16:13:17 -0800

    Add project files.

---
+++ imageConvert2BMP/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(imageConvert2BMP.Startup))]
+namespace imageConvert2BMP
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            
+        }
+    }
+}
